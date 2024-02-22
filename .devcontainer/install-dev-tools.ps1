fa38657196b4940455eced4f7c416dbf1a4899c3
YAEGASHI Takeshi <yaegashi@gmail.com>
2024-02-22 18:30:18 +0000

    Update devcontainer.json

---
+++ .devcontainer/install-dev-tools.ps1
@@ -0,0 +1,4 @@
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name Microsoft.Graph.Authentication
+Install-Module -Name Microsoft.Graph.Users
+Install-Module -Name Microsoft.Graph.Groups
