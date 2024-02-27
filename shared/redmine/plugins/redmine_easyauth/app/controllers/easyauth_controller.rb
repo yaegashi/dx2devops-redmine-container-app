2734db7858d19d4ff581794c9e5d073244787bfc
YAEGASHI Takeshi <yaegashi@gmail.com>
2024-02-27 23:52:58 +0000

    shared: redmine: update redmine_easyauth plugin to v0.0.2
    Show login field, email field, account switch link in the login page

--- shared/redmine/plugins/redmine_easyauth/app/controllers/easyauth_controller.rb
+++ shared/redmine/plugins/redmine_easyauth/app/controllers/easyauth_controller.rb
@@ -1,10 +1,5 @@
 class EasyauthController < AccountController
-  # Easy Auth: Extract user principal information from request headers
-  # https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-user-identities
-  # X-MS-CLIENT-PRINCIPAL: Base64 encoded JSON object
-  # X-MS-CLIENT-PRINCIPAL-NAME: User's mail address
-  PRINCIPAL_HEADER = 'HTTP_X_MS_CLIENT_PRINCIPAL'.freeze
-  NAME_HEADER = 'HTTP_X_MS_CLIENT_PRINCIPAL_NAME'.freeze
+  include RedmineEasyauthHelper
 
   def easyauth_failure
     flash['error'] = "#{l('easyauth.error.authentication_failed')}: #{params[:message]}"
@@ -18,36 +13,12 @@ class EasyauthController < AccountController
       return
     end
 
-    name = ENV[NAME_HEADER] || request.headers[NAME_HEADER]
-    logger.info "easyauth name: #{name.inspect}"
-
-    principal_raw = ENV[PRINCIPAL_HEADER] || request.headers[PRINCIPAL_HEADER]
-    logger.info "easyauth principal raw: #{principal_raw.inspect}"
-
-    begin
-      principal = JSON.parse(Base64.decode64(principal_raw))
-      raise 'not a hash' unless principal.is_a?(Hash)
-    rescue => e
-      logger.error "easyauth principal decode error: #{e}"
-      principal = {}
-    end
-    logger.info "easyauth principal decoded: #{principal.inspect}"
-
-    if name.blank?
+    login, name, claims = easyauth_claims
+    if login.blank? or name.blank?
       logger.info 'easyauth unavailable'
       flash['error'] = l('easyauth.error.authentication_unavailable')
       redirect_to signin_path
       return
-    end
-
-    claims = {}
-    if principal['auth_typ'] == 'aad'
-      principal.fetch('claims', []).each do |claim|
-        k = claim['typ'].to_s
-        v = claim['val'].to_s
-        next if k.blank? || v.blank?
-        claims[k] = claims.fetch(k, []) + [v]
-      end
     end
 
     oid1 = claims.fetch('groups', [])
